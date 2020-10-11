32f51577565c7a12071fe89cc2f5810c9a02a288
Igor Ivanov <guver3g@gmail.com>
2020-10-11 21:11:27 +0300

    update

--- homework2/src/main/java/by/itacademy/homework2/SecondActivity.java
+++ homework2/src/main/java/by/itacademy/homework2/SecondActivity.java
@@ -1,7 +1,9 @@
 package by.itacademy.homework2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
