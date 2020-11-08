95e833acfc5cfcd3a8414666f797d9bc0fe77ee3
Igor Ivanov <guver3g@gmail.com>
2020-11-08 12:24:19 +0300

    started hm6

---
+++ homework6/src/main/java/by/itacademy/homework6/TextRedactorActivity.kt
@@ -0,0 +1,11 @@
+package by.itacademy.homework6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextRedactorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_redactor)
+    }
+}
